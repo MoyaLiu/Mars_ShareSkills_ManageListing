b51d53fdb304462c20b0d9db9b01202ed2f7a13c
Moya <llp0316@gmail.com>
2020-10-06 12:46:39 +1300

    1. Config the res. 2. Implement signin

---
+++ marsframework-master/MarsFramework/Pages/SignIn.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using MarsFramework.Config;
+using MarsFramework.Global;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace MarsFramework.Pages
+{
+    class SignIn
+    {
+        public SignIn()
+        {
+            PageFactory.InitElements(Global.GlobalDefinitions.driver, this);
+        }
+
+        #region  Initialize Web Elements 
+        //Finding the Sign Link
+        [FindsBy(How = How.XPath, Using = "//a[contains(text(),'Sign')]")]
+        private IWebElement SignIntab { get; set; }
+
+        // Finding the Email Field
+        [FindsBy(How = How.Name, Using = "email")]
+        private IWebElement Email { get; set; }
+
+        //Finding the Password Field
+        [FindsBy(How = How.Name, Using = "password")]
+        private IWebElement Password { get; set; }
+
+        //Finding the Login Button
+        [FindsBy(How = How.XPath, Using = "//button[contains(text(),'Login')]")]
+        private IWebElement LoginBtn { get; set; }
+
+        #endregion
+
+        internal void LoginSteps()
+        {
+
+            GlobalDefinitions.ExcelLib.PopulateInCollection(MarsResource.ExcelPath, "SignIn");
+            GlobalDefinitions.driver.Navigate().GoToUrl(GlobalDefinitions.ExcelLib.ReadData(2, "Url"));
+            GlobalDefinitions.driver.Manage().Window.Maximize();
+
+            SignIntab.Click();
+
+            Email.Click();
+            Email.Clear();
+            Console.WriteLine("username = " + GlobalDefinitions.ExcelLib.ReadData(2, "Username"));
+            Email.SendKeys(GlobalDefinitions.ExcelLib.ReadData(2, "Username"));
+            
+            Password.Click();
+            Password.Clear();
+            Password.SendKeys(GlobalDefinitions.ExcelLib.ReadData(2, "Password"));
+            Console.WriteLine("password = " + GlobalDefinitions.ExcelLib.ReadData(2, "Password"));
+
+            LoginBtn.Click();
+
+
+        }
+    }
+}
